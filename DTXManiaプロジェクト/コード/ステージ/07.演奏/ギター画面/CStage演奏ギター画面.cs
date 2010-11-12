5104dbd3ad4bfd6557193baec2e20dbbb44b757a
yyagi <yyagi@16f42ceb-6dc6-49c8-ba94-f2d53467949d>
2010-11-12 14:21:31 +0000

    #23630 ギター・ベース譜面がどちらもない場合にはギタレボモードを即閉店とするよう修正。
    git-svn-id: http://svn.osdn.jp/svnroot/dtxmania/trunk@33 16f42ceb-6dc6-49c8-ba94-f2d53467949d

--- DTXManiaプロジェクト/コード/ステージ/07.演奏/ギター画面/CStage演奏ギター画面.cs
+++ DTXManiaプロジェクト/コード/ステージ/07.演奏/ギター画面/CStage演奏ギター画面.cs
@@ -250,8 +250,8 @@ namespace DTXMania
 				{
 //					bool flag3 = ( CDTXMania.ConfigIni.bAutoPlay.Guitar || !CDTXMania.DTX.bチップがある.Guitar ) || ( this.actGauge.db現在のゲージ値.Guitar <= -0.1 );				// #23630
 //					bool flag4 = ( CDTXMania.ConfigIni.bAutoPlay.Bass || !CDTXMania.DTX.bチップがある.Bass ) || ( this.actGauge.db現在のゲージ値.Bass <= -0.1 );					// #23630
-					bool bFailedGuitar = (!CDTXMania.ConfigIni.bAutoPlay.Guitar && ((!CDTXMania.DTX.bチップがある.Guitar) || (this.actGauge.db現在のゲージ値.Guitar <= -0.1)));		// #23630 2011.11.9 yyagi: separated AutoPlay condition: not to be failed at once
-					bool bFailedBass   = (!CDTXMania.ConfigIni.bAutoPlay.Bass   && ((!CDTXMania.DTX.bチップがある.Bass)   || (this.actGauge.db現在のゲージ値.Bass   <= -0.1)));		// #23630 
+					bool bFailedGuitar = ((!CDTXMania.DTX.bチップがある.Guitar) || (this.actGauge.db現在のゲージ値.Guitar <= -0.1));		// #23630 2011.11.12 yyagi: deleted AutoPlay condition: not to be failed at once
+					bool bFailedBass   = ((!CDTXMania.DTX.bチップがある.Bass)   || (this.actGauge.db現在のゲージ値.Bass   <= -0.1));		// #23630 
 					if (bFailedGuitar && bFailedBass)
 					{
 						this.actStageFailed.Start();
