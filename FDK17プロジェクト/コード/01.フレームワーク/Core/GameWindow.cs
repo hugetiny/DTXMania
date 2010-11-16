16f23e43451ee2b471ecc8c5b557ce4439b5f780
yyagi <yyagi@16f42ceb-6dc6-49c8-ba94-f2d53467949d>
2010-11-16 15:30:31 +0000

    #23510 ウインドウの縦横比維持リサイズ: ウインドウの枠の大きさを配慮するようにした。これで一旦対応完了とします。
    git-svn-id: http://svn.osdn.jp/svnroot/dtxmania/trunk@39 16f42ceb-6dc6-49c8-ba94-f2d53467949d

--- FDK17プロジェクト/コード/01.フレームワーク/Core/GameWindow.cs
+++ FDK17プロジェクト/コード/01.フレームワーク/Core/GameWindow.cs
@@ -386,30 +386,45 @@ namespace SampleFramework
 				#endregion
 			}
 
-			#region #23510 2010.11.14 yyagi add: 縦横比固定でのウインドウサイズ変更 from http://www.vcskicks.com/maintain-aspect-ratio.php
+			#region #23510 2010.11.16 yyagi add: 縦横比固定でのウインドウサイズ変更 from http://www.vcskicks.com/maintain-aspect-ratio.php
 			else if (m.Msg == WM_SIZING)
 			{
 				RECT rc = (RECT)Marshal.PtrToStructure(m.LParam, typeof(RECT));
 				int res = m.WParam.ToInt32();
+				int totalWidth = rc.Right - rc.Left;
+				int totalHeight = rc.Bottom - rc.Top;
+				int edgeWidth = totalWidth - this.ClientSize.Width;
+				int edgeHeight = totalHeight - this.ClientSize.Height;
+
 				if (res == WMSZ_LEFT || res == WMSZ_RIGHT)
 				{
 					//Left or right resize -> adjust height (bottom)
-					rc.Bottom = rc.Top + (int)(heightRatio * this.Width / widthRatio);
+					rc.Bottom = rc.Top + (int)(heightRatio * this.ClientSize.Width / widthRatio) + edgeHeight;
 				}
 				else if (res == WMSZ_TOP || res == WMSZ_BOTTOM)
 				{
 					//Up or down resize -> adjust width (right)
-					rc.Right = rc.Left + (int)(widthRatio * this.Height / heightRatio);
+					rc.Right = rc.Left + (int)(widthRatio * this.ClientSize.Height / heightRatio) + edgeWidth;
 				}
 				else if (res == WMSZ_RIGHT + WMSZ_BOTTOM)
 				{
 					//Lower-right corner resize -> adjust height (could have been width)
-					rc.Bottom = rc.Top + (int)(heightRatio * this.Width / widthRatio);
+					rc.Bottom = rc.Top + (int)(heightRatio * this.ClientSize.Width / widthRatio) + edgeHeight;
 				}
 				else if (res == WMSZ_LEFT + WMSZ_TOP)
 				{
 					//Upper-left corner -> adjust width (could have been height)
-					rc.Left = rc.Right - (int)(widthRatio * this.Height / heightRatio);
+					rc.Left = rc.Right - (int)(widthRatio * this.ClientSize.Height / heightRatio) + edgeWidth;
+				}
+				else if (res == WMSZ_RIGHT + WMSZ_TOP)
+				{
+					//Upper-right corner resize -> adjust height (could have been width)
+					rc.Bottom = rc.Top + (int)(heightRatio * this.ClientSize.Width / widthRatio) + edgeHeight;
+				}
+				else if (res == WMSZ_LEFT + WMSZ_BOTTOM)
+				{
+					//Lower-left corner -> adjust width (could have been height)
+					rc.Left = rc.Right - (int)(widthRatio * this.ClientSize.Height / heightRatio) + edgeWidth;
 				}
 				Marshal.StructureToPtr(rc, m.LParam, true);
 			}
