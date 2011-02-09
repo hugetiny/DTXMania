5c1f081eaa77ff641d029860f6ae10a7da1b6e7d
yyagi <yyagi@16f42ceb-6dc6-49c8-ba94-f2d53467949d>
2011-02-09 13:54:33 +0000

    DTXC: ファイルのバージョン情報を更新し忘れていたので修正。
    git-svn-id: http://svn.osdn.jp/svnroot/dtxmania/trunk@109 16f42ceb-6dc6-49c8-ba94-f2d53467949d

--- DTXCreatorプロジェクト/Properties/AssemblyInfo.cs
+++ DTXCreatorプロジェクト/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Resources;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("22.0.0.0")]
-[assembly: AssemblyFileVersion("22.0.0.0")]
+[assembly: AssemblyVersion( "23.0.0.0" )]
+[assembly: AssemblyFileVersion( "23.0.0.0" )]
 [assembly: NeutralResourcesLanguageAttribute("")]
