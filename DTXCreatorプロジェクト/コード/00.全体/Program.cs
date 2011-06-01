1e72e93f87447dad43214e901f5f650918135092
yyagi <yyagi@16f42ceb-6dc6-49c8-ba94-f2d53467949d>
2011-06-01 15:48:50 +0000

    #none [DTXC] added USE_GERMANRESOURCE macro to test German locales in Japan.
    git-svn-id: http://svn.osdn.jp/svnroot/dtxmania/trunk@184 16f42ceb-6dc6-49c8-ba94-f2d53467949d

--- DTXCreatorプロジェクト/コード/00.全体/Program.cs
+++ DTXCreatorプロジェクト/コード/00.全体/Program.cs
@@ -14,6 +14,8 @@ namespace DTXCreator
 #if DEBUG
 #if USE_ENGLISHRESOURCE
 			Thread.CurrentThread.CurrentUICulture = new CultureInfo( "en-GB", false );	// yyagi; For testing English resources
+#elif USE_GERMANRESOURCE
+			Thread.CurrentThread.CurrentCulture = new CultureInfo( "de-DE", false );	// yyagi; For testing decimal point in German resources
 #endif
 #endif
 			Application.EnableVisualStyles();
