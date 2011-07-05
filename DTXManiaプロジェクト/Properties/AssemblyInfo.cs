595c3e68cab09036de34e8d6f7966d64c472ba82
yyagi <yyagi@16f42ceb-6dc6-49c8-ba94-f2d53467949d>
2011-07-05 14:28:52 +0000

    #none アセンブリ情報を更新。
    git-svn-id: http://svn.osdn.jp/svnroot/dtxmania/trunk@219 16f42ceb-6dc6-49c8-ba94-f2d53467949d

--- DTXManiaプロジェクト/Properties/AssemblyInfo.cs
+++ DTXManiaプロジェクト/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Resources;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "90.0.0.0" )]
-[assembly: AssemblyFileVersion( "90.0.0.0" )]
+[assembly: AssemblyVersion("91.0.0.0")]
+[assembly: AssemblyFileVersion("91.0.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("ja-JP")]
 
 // このアセンブリは「ライブラリ」である（難読化ツールへの指示）。
