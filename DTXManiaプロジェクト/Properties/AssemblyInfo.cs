49a43aeb71604abdb8d3791fba1e6130c4d1546c
yyagi <yyagi@16f42ceb-6dc6-49c8-ba94-f2d53467949d>
2012-01-03 05:36:58 +0000

    092でのバージョン情報のコミット漏れ。
    git-svn-id: http://svn.osdn.jp/svnroot/dtxmania/trunk@276 16f42ceb-6dc6-49c8-ba94-f2d53467949d

--- DTXManiaプロジェクト/Properties/AssemblyInfo.cs
+++ DTXManiaプロジェクト/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Resources;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("91.0.0.1")]
-[assembly: AssemblyFileVersion("91.0.0.1")]
+[assembly: AssemblyVersion( "92.0.0.0" )]
+[assembly: AssemblyFileVersion( "92.0.0.0" )]
 [assembly: NeutralResourcesLanguageAttribute("ja-JP")]
 
 // このアセンブリは「ライブラリ」である（難読化ツールへの指示）。
--- DTXManiaプロジェクト/コード/全体/CDTXMania.cs
+++ DTXManiaプロジェクト/コード/全体/CDTXMania.cs
@@ -19,7 +19,7 @@ namespace DTXMania
 	{
 		// プロパティ
 
-		public static readonly string VERSION = "091(110709)";
+		public static readonly string VERSION = "092(111223)";
 //		public static readonly string VERSION = "086(101120)";
 		public static readonly string SLIMDXDLL = "c_net20x86_Jun2010";
 		public static readonly string D3DXDLL = "d3dx9_43.dll";		// June 2010
