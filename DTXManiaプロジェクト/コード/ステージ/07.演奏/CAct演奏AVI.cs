2c5aecb8fc81752064e11f40fdd0382807052b23
yyagi <yyagi@16f42ceb-6dc6-49c8-ba94-f2d53467949d>
2014-02-08 13:55:42 +0000

    #28821 CAct演奏AVI.cs をさらにロールバックし、rev760相当に戻した。これまでの#28821への対応で使用した、余計な修正を省くため。
    git-svn-id: http://svn.osdn.jp/svnroot/dtxmania/branches/140113(DTXMania098%20with%2028821%20DTXVmode)@681 16f42ceb-6dc6-49c8-ba94-f2d53467949d

--- DTXManiaプロジェクト/コード/ステージ/07.演奏/CAct演奏AVI.cs
+++ DTXManiaプロジェクト/コード/ステージ/07.演奏/CAct演奏AVI.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Drawing;
-using System.Diagnostics;
-using System.Runtime.InteropServices;
 using SlimDX;
 using SlimDX.Direct3D9;
 using FDK;
@@ -17,13 +15,14 @@ namespace DTXMania
 		public CAct演奏AVI()
 		{
 			base.b活性化してない = true;
-		} 
+		}
+
 
 		// メソッド
 
-		public void Start( int nチャンネル番号, CDTX.CAVI rAVI, int n開始サイズW, int n開始サイズH, int n終了サイズW, int n終了サイズH, int n画像側開始位置X, int n画像側開始位置Y, int n画像側終了位置X, int n画像側終了位置Y, int n表示側開始位置X, int n表示側開始位置Y, int n表示側終了位置X, int n表示側終了位置Y, int n総移動時間ms, int n表示開始時刻ms )
+		public void Start( int nチャンネル番号, CDTX.CAVI rAVI, int n開始サイズW, int n開始サイズH, int n終了サイズW, int n終了サイズH, int n画像側開始位置X, int n画像側開始位置Y, int n画像側終了位置X, int n画像側終了位置Y, int n表示側開始位置X, int n表示側開始位置Y, int n表示側終了位置X, int n表示側終了位置Y, int n総移動時間ms, int n移動開始時刻ms )
 		{
-			if( nチャンネル番号 == 0x54 )
+			if ( nチャンネル番号 == 0x54 )
 			{
 				this.rAVI = rAVI;
 				this.n開始サイズW = n開始サイズW;
@@ -39,31 +38,31 @@ namespace DTXMania
 				this.n表示側終了位置X = n表示側終了位置X;
 				this.n表示側終了位置Y = n表示側終了位置Y;
 				this.n総移動時間ms = n総移動時間ms;
-				this.n表示開始時刻ms = ( n表示開始時刻ms != -1 ) ? n表示開始時刻ms : CSound管理.rc演奏用タイマ.n現在時刻;
+				this.n移動開始時刻ms = ( n移動開始時刻ms != -1 ) ? n移動開始時刻ms : CSound管理.rc演奏用タイマ.n現在時刻;
 				this.n前回表示したフレーム番号 = -1;
 			}
 		}
 		public void SkipStart( int n移動開始時刻ms )
 		{
-			foreach( CDTX.CChip chip in CDTXMania.DTX.listChip )
+			foreach ( CDTX.CChip chip in CDTXMania.DTX.listChip )
 			{
-				if( chip.n発声時刻ms > n移動開始時刻ms )
+				if ( chip.n発声時刻ms > n移動開始時刻ms )
 				{
 					break;
 				}
-				switch( chip.eAVI種別 )
+				switch ( chip.eAVI種別 )
 				{
 					case EAVI種別.AVI:
 						{
-							if( chip.rAVI != null )
+							if ( chip.rAVI != null )
 							{
-								this.Start( chip.nチャンネル番号, chip.rAVI, 278, 355, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, chip.n発声時刻ms );
+								this.Start( chip.nチャンネル番号, chip.rAVI, 0x116, 0x163, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, chip.n発声時刻ms );
 							}
 							continue;
 						}
 					case EAVI種別.AVIPAN:
 						{
-							if( chip.rAVIPan != null )
+							if ( chip.rAVIPan != null )
 							{
 								this.Start( chip.nチャンネル番号, chip.rAVI, chip.rAVIPan.sz開始サイズ.Width, chip.rAVIPan.sz開始サイズ.Height, chip.rAVIPan.sz終了サイズ.Width, chip.rAVIPan.sz終了サイズ.Height, chip.rAVIPan.pt動画側開始位置.X, chip.rAVIPan.pt動画側開始位置.Y, chip.rAVIPan.pt動画側終了位置.X, chip.rAVIPan.pt動画側終了位置.Y, chip.rAVIPan.pt表示側開始位置.X, chip.rAVIPan.pt表示側開始位置.Y, chip.rAVIPan.pt表示側終了位置.X, chip.rAVIPan.pt表示側終了位置.Y, chip.n総移動時間, chip.n発声時刻ms );
 							}
@@ -74,99 +73,86 @@ namespace DTXMania
 		}
 		public void Stop()
 		{
-			if( ( this.rAVI != null ) && ( this.rAVI.avi != null ) )
+			if ( ( this.rAVI != null ) && ( this.rAVI.avi != null ) )
 			{
-				this.n表示開始時刻ms = -1;
+				this.n移動開始時刻ms = -1;
 			}
 		}
 		public void Cont( int n再開時刻ms )
 		{
-			if( ( this.rAVI != null ) && ( this.rAVI.avi != null ) )
+			if ( ( this.rAVI != null ) && ( this.rAVI.avi != null ) )
 			{
-				this.n表示開始時刻ms = n再開時刻ms;
+				this.n移動開始時刻ms = n再開時刻ms;
 			}
 		}
-
 		public unsafe int t進行描画( int x, int y )
 		{
-			if( !base.b活性化してない )
+			if ( !base.b活性化してない )
 			{
 				Rectangle rectangle;
 				Rectangle rectangle2;
-				if( ( ( this.n表示開始時刻ms == -1 ) || ( this.rAVI == null ) ) || ( this.rAVI.avi == null ) )
+				if ( ( ( this.n移動開始時刻ms == -1 ) || ( this.rAVI == null ) ) || ( this.rAVI.avi == null ) )
 				{
 					return 0;
 				}
-				if( this.tx描画用 == null )
+				if ( this.tx描画用 == null )
 				{
 					return 0;
 				}
-				int time = (int) ( ( CSound管理.rc演奏用タイマ.n現在時刻 - this.n表示開始時刻ms ) * ( ( (double) CDTXMania.ConfigIni.n演奏速度 ) / 20.0 ) );
-//Trace.TraceInformation( "n現在時刻=" + CSound管理.rc演奏用タイマ.n現在時刻 +  ", n表示開始時刻ms=" + n表示開始時刻ms + ", time=" + time　);
-				if ( time < 0 )
-				{
-					return 0; 
-				}
+				int time = (int) ( ( CSound管理.rc演奏用タイマ.n現在時刻 - this.n移動開始時刻ms ) * ( ( (double) CDTXMania.ConfigIni.n演奏速度 ) / 20.0 ) );
 				int frameNoFromTime = this.rAVI.avi.GetFrameNoFromTime( time );
-				if( ( this.n総移動時間ms != 0 ) && ( this.n総移動時間ms < time ) )
+				if ( ( this.n総移動時間ms != 0 ) && ( this.n総移動時間ms < time ) )
 				{
 					this.n総移動時間ms = 0;
-					this.n表示開始時刻ms = -1;
+					this.n移動開始時刻ms = -1;
 					return 0;
 				}
-				if( ( this.n総移動時間ms == 0 ) && ( frameNoFromTime >= this.rAVI.avi.GetMaxFrameCount() ) )
+				if ( ( this.n総移動時間ms == 0 ) && ( frameNoFromTime >= this.rAVI.avi.GetMaxFrameCount() ) )
 				{
-					this.n表示開始時刻ms = -1;
+					this.n移動開始時刻ms = -1;
 					return 0;
 				}
-				if( ( this.n前回表示したフレーム番号 != frameNoFromTime ) && !this.bフレームを作成した )
+				if ( ( this.n前回表示したフレーム番号 != frameNoFromTime ) && !this.bフレームを作成した )
 				{
-					//this.pBmp = this.rAVI.avi.GetFramePtr( frameNoFromTime );
+					this.pBmp = this.rAVI.avi.GetFramePtr( frameNoFromTime );
 					this.n前回表示したフレーム番号 = frameNoFromTime;
 					this.bフレームを作成した = true;
 				}
 				Size size = new Size( (int) this.rAVI.avi.nフレーム幅, (int) this.rAVI.avi.nフレーム高さ );
-				Size size2 = new Size( 278, 355 );
+				Size size2 = new Size( 0x116, 0x163 );
 				Size size3 = new Size( this.n開始サイズW, this.n開始サイズH );
 				Size size4 = new Size( this.n終了サイズW, this.n終了サイズH );
 				Point location = new Point( this.n画像側開始位置X, this.n画像側終了位置Y );
 				Point point2 = new Point( this.n画像側終了位置X, this.n画像側終了位置Y );
 				Point point3 = new Point( this.n表示側開始位置X, this.n表示側開始位置Y );
 				Point point4 = new Point( this.n表示側終了位置X, this.n表示側終了位置Y );
-
-	
-				
-				long _n表示開始時刻 = this.n表示開始時刻ms;
-				if ( CSound管理.rc演奏用タイマ.n現在時刻 < this.n表示開始時刻ms )
+				long num3 = this.n総移動時間ms;
+				long num4 = this.n移動開始時刻ms;
+				if ( CSound管理.rc演奏用タイマ.n現在時刻 < num4 )
 				{
-					_n表示開始時刻 = CSound管理.rc演奏用タイマ.n現在時刻;
+					num4 = CSound管理.rc演奏用タイマ.n現在時刻;
 				}
-				time = (int) ( ( CSound管理.rc演奏用タイマ.n現在時刻 - _n表示開始時刻 ) * ( ( (double) CDTXMania.ConfigIni.n演奏速度 ) / 20.0 ) );
-
-				#region [ AVI ]
-				long _n総移動時間 = this.n総移動時間ms;
-				if ( _n総移動時間 == 0 )
+				time = (int) ( ( CSound管理.rc演奏用タイマ.n現在時刻 - num4 ) * ( ( (double) CDTXMania.ConfigIni.n演奏速度 ) / 20.0 ) );
+				if ( num3 == 0 )
 				{
 					rectangle = new Rectangle( location, size3 );
 					rectangle2 = new Rectangle( point3, size3 );
 				}
-				#endregion
 				else
-				#region [ AVIPAN ]
 				{
-					double num5 = ( (double) time ) / ( (double) _n総移動時間 );
+					double num5 = ( (double) time ) / ( (double) num3 );
 					Size size5 = new Size( size3.Width + ( (int) ( ( size4.Width - size3.Width ) * num5 ) ), size3.Height + ( (int) ( ( size4.Height - size3.Height ) * num5 ) ) );
 					rectangle = new Rectangle( (int) ( ( point2.X - location.X ) * num5 ), (int) ( ( point2.Y - location.Y ) * num5 ), ( (int) ( ( point2.X - location.X ) * num5 ) ) + size5.Width, ( (int) ( ( point2.Y - location.Y ) * num5 ) ) + size5.Height );
 					rectangle2 = new Rectangle( (int) ( ( point4.X - point3.X ) * num5 ), (int) ( ( point4.Y - point3.Y ) * num5 ), ( (int) ( ( point4.X - point3.X ) * num5 ) ) + size5.Width, ( (int) ( ( point4.Y - point3.Y ) * num5 ) ) + size5.Height );
-					if( ( ( rectangle.Right <= 0 ) || ( rectangle.Bottom <= 0 ) ) || ( ( rectangle.Left >= size.Width ) || ( rectangle.Top >= size.Height ) ) )
+					if ( ( ( rectangle.Right <= 0 ) || ( rectangle.Bottom <= 0 ) ) || ( ( rectangle.Left >= size.Width ) || ( rectangle.Top >= size.Height ) ) )
 					{
 						return 0;
 					}
-					if( ( ( rectangle2.Right <= 0 ) || ( rectangle2.Bottom <= 0 ) ) || ( ( rectangle2.Left >= size2.Width ) || ( rectangle2.Top >= size2.Height ) ) )
+					if ( ( ( rectangle2.Right <= 0 ) || ( rectangle2.Bottom <= 0 ) ) || ( ( rectangle2.Left >= size2.Width ) || ( rectangle2.Top >= size2.Height ) ) )
 					{
 						return 0;
 					}
-					if( rectangle.X < 0 )
+					if ( rectangle.X < 0 )
 					{
 						int num6 = -rectangle.X;
 						rectangle2.X += num6;
@@ -174,7 +160,7 @@ namespace DTXMania
 						rectangle.X = 0;
 						rectangle.Width -= num6;
 					}
-					if( rectangle.Y < 0 )
+					if ( rectangle.Y < 0 )
 					{
 						int num7 = -rectangle.Y;
 						rectangle2.Y += num7;
@@ -182,19 +168,19 @@ namespace DTXMania
 						rectangle.Y = 0;
 						rectangle.Height -= num7;
 					}
-					if( rectangle.Right > size.Width )
+					if ( rectangle.Right > size.Width )
 					{
 						int num8 = rectangle.Right - size.Width;
 						rectangle2.Width -= num8;
 						rectangle.Width -= num8;
 					}
-					if( rectangle.Bottom > size.Height )
+					if ( rectangle.Bottom > size.Height )
 					{
 						int num9 = rectangle.Bottom - size.Height;
 						rectangle2.Height -= num9;
 						rectangle.Height -= num9;
 					}
-					if( rectangle2.X < 0 )
+					if ( rectangle2.X < 0 )
 					{
 						int num10 = -rectangle2.X;
 						rectangle.X += num10;
@@ -202,7 +188,7 @@ namespace DTXMania
 						rectangle2.X = 0;
 						rectangle2.Width -= num10;
 					}
-					if( rectangle2.Y < 0 )
+					if ( rectangle2.Y < 0 )
 					{
 						int num11 = -rectangle2.Y;
 						rectangle.Y += num11;
@@ -210,66 +196,55 @@ namespace DTXMania
 						rectangle2.Y = 0;
 						rectangle2.Height -= num11;
 					}
-					if( rectangle2.Right > size2.Width )
+					if ( rectangle2.Right > size2.Width )
 					{
 						int num12 = rectangle2.Right - size2.Width;
 						rectangle.Width -= num12;
 						rectangle2.Width -= num12;
 					}
-					if( rectangle2.Bottom > size2.Height )
+					if ( rectangle2.Bottom > size2.Height )
 					{
 						int num13 = rectangle2.Bottom - size2.Height;
 						rectangle.Height -= num13;
 						rectangle2.Height -= num13;
 					}
-					if( ( rectangle.X >= rectangle.Right ) || ( rectangle.Y >= rectangle.Bottom ) )
+					if ( ( rectangle.X >= rectangle.Right ) || ( rectangle.Y >= rectangle.Bottom ) )
 					{
 						return 0;
 					}
-					if( ( rectangle2.X >= rectangle2.Right ) || ( rectangle2.Y >= rectangle2.Bottom ) )
+					if ( ( rectangle2.X >= rectangle2.Right ) || ( rectangle2.Y >= rectangle2.Bottom ) )
 					{
 						return 0;
 					}
-					if( ( ( rectangle.Right < 0 ) || ( rectangle.Bottom < 0 ) ) || ( ( rectangle.X > size.Width ) || ( rectangle.Y > size.Height ) ) )
+					if ( ( ( rectangle.Right < 0 ) || ( rectangle.Bottom < 0 ) ) || ( ( rectangle.X > size.Width ) || ( rectangle.Y > size.Height ) ) )
 					{
 						return 0;
 					}
-					if( ( ( rectangle2.Right < 0 ) || ( rectangle2.Bottom < 0 ) ) || ( ( rectangle2.X > size2.Width ) || ( rectangle2.Y > size2.Height ) ) )
+					if ( ( ( rectangle2.Right < 0 ) || ( rectangle2.Bottom < 0 ) ) || ( ( rectangle2.X > size2.Width ) || ( rectangle2.Y > size2.Height ) ) )
 					{
 						return 0;
 					}
 				}
-				#endregion
-				if( ( this.tx描画用 != null ) && ( this.n総移動時間ms != -1 ) )
+				if ( ( this.tx描画用 != null ) && ( this.n総移動時間ms != -1 ) )
 				{
-					if( this.bフレームを作成した )
+					if ( this.bフレームを作成した && ( this.pBmp != IntPtr.Zero ) )
 					{
-						// unsafe中のローカル変数は、スタックに固定される(GCで動かされない)
-						// -> this.pBmpでなく、ローカルのpBmpを使う
-
-						IntPtr pBmp = this.rAVI.avi.GetFramePtr( frameNoFromTime );
+						DataRectangle rectangle3 = this.tx描画用.texture.LockRectangle( 0, LockFlags.None );
+						DataStream data = rectangle3.Data;
+						int num14 = rectangle3.Pitch / this.tx描画用.szテクスチャサイズ.Width;
+						BitmapUtil.BITMAPINFOHEADER* pBITMAPINFOHEADER = (BitmapUtil.BITMAPINFOHEADER*) this.pBmp.ToPointer();
+						if ( pBITMAPINFOHEADER->biBitCount == 0x18 )
 						{
-							if ( pBmp != IntPtr.Zero )
+							switch ( num14 )
 							{
-								DataRectangle rectangle3 = this.tx描画用.texture.LockRectangle( 0, LockFlags.None );
-								DataStream data = rectangle3.Data;
-								int num14 = rectangle3.Pitch / this.tx描画用.szテクスチャサイズ.Width;
-								BitmapUtil.BITMAPINFOHEADER* pBITMAPINFOHEADER = (BitmapUtil.BITMAPINFOHEADER*) pBmp.ToPointer();
-								if ( pBITMAPINFOHEADER->biBitCount == 24 )
-								{
-									switch ( num14 )
-									{
-										case 2:
-											this.rAVI.avi.tBitmap24ToGraphicsStreamR5G6B5( pBITMAPINFOHEADER, data, this.tx描画用.szテクスチャサイズ.Width, this.tx描画用.szテクスチャサイズ.Height );
-											break;
+								case 2:
+									this.rAVI.avi.tBitmap24ToGraphicsStreamR5G6B5( pBITMAPINFOHEADER, data, this.tx描画用.szテクスチャサイズ.Width, this.tx描画用.szテクスチャサイズ.Height );
+									break;
 
-										case 4:
-											this.rAVI.avi.tBitmap24ToGraphicsStreamX8R8G8B8( pBITMAPINFOHEADER, data, this.tx描画用.szテクスチャサイズ.Width, this.tx描画用.szテクスチャサイズ.Height );
-											break;
-									}
-								}
+								case 4:
+									this.rAVI.avi.tBitmap24ToGraphicsStreamX8R8G8B8( pBITMAPINFOHEADER, data, this.tx描画用.szテクスチャサイズ.Width, this.tx描画用.szテクスチャサイズ.Height );
+									break;
 							}
-
 						}
 						this.tx描画用.texture.UnlockRectangle( 0 );
 						this.bフレームを作成した = false;
@@ -280,21 +255,21 @@ namespace DTXMania
 			return 0;
 		}
 
-		
+
 		// CActivity 実装
 
 		public override void On活性化()
 		{
 			this.rAVI = null;
-			this.n表示開始時刻ms = -1;
+			this.n移動開始時刻ms = -1;
 			this.n前回表示したフレーム番号 = -1;
 			this.bフレームを作成した = false;
-		//	this.pBmp = IntPtr.Zero;
+			this.pBmp = IntPtr.Zero;
 			base.On活性化();
 		}
 		public override void OnManagedリソースの作成()
 		{
-			if( !base.b活性化してない )
+			if ( !base.b活性化してない )
 			{
 #if TEST_Direct3D9Ex
 				this.tx描画用 = new CTexture( CDTXMania.app.Device, 320, 355, CDTXMania.app.GraphicsDeviceManager.CurrentSettings.BackBufferFormat, Pool.Default, Usage.Dynamic );
@@ -306,9 +281,9 @@ namespace DTXMania
 		}
 		public override void OnManagedリソースの解放()
 		{
-			if( !base.b活性化してない )
+			if ( !base.b活性化してない )
 			{
-				if( this.tx描画用 != null )
+				if ( this.tx描画用 != null )
 				{
 					this.tx描画用.Dispose();
 					this.tx描画用 = null;
@@ -327,7 +302,7 @@ namespace DTXMania
 		#region [ private ]
 		//-----------------
 		private bool bフレームを作成した;
-		private long n表示開始時刻ms;
+		private long n移動開始時刻ms;
 		private int n画像側開始位置X;
 		private int n画像側開始位置Y;
 		private int n画像側終了位置X;
@@ -342,7 +317,7 @@ namespace DTXMania
 		private int n表示側開始位置Y;
 		private int n表示側終了位置X;
 		private int n表示側終了位置Y;
-//		private IntPtr pBmp;
+		private IntPtr pBmp;
 		private CDTX.CAVI rAVI;
 		private CTexture tx描画用;
 		//-----------------
