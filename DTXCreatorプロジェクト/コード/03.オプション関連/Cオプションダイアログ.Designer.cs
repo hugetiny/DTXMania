c7dd54f3c82311c41f3fb57ca96c6809243d699f
yyagi <yyagi@16f42ceb-6dc6-49c8-ba94-f2d53467949d>
2014-07-05 00:21:19 +0000

    #28821 DTXCのviewer指定画面で、TimeStretchはWASAPI/ASIO時のみ使用可能であることを補足追記した。
    git-svn-id: http://svn.osdn.jp/svnroot/dtxmania/branches/140113(DTXMania098%20with%2028821%20DTXVmode)@729 16f42ceb-6dc6-49c8-ba94-f2d53467949d

--- DTXCreatorプロジェクト/コード/03.オプション関連/Cオプションダイアログ.Designer.cs
+++ DTXCreatorプロジェクト/コード/03.オプション関連/Cオプションダイアログ.Designer.cs
@@ -69,74 +69,48 @@
 			// 
 			// tabPage全般
 			// 
-			this.tabPage全般.AccessibleDescription = null;
-			this.tabPage全般.AccessibleName = null;
-			resources.ApplyResources( this.tabPage全般, "tabPage全般" );
-			this.tabPage全般.BackgroundImage = null;
 			this.tabPage全般.Controls.Add( this.checkBoxPlaySoundOnChip );
 			this.tabPage全般.Controls.Add( this.checkBoxPreviewBGM );
 			this.tabPage全般.Controls.Add( this.checkBoxオートフォーカス );
 			this.tabPage全般.Controls.Add( this.label個まで表示する );
 			this.tabPage全般.Controls.Add( this.checkBox最近使用したファイル );
 			this.tabPage全般.Controls.Add( this.numericUpDown最近使用したファイルの最大表示個数 );
-			this.tabPage全般.Font = null;
+			resources.ApplyResources( this.tabPage全般, "tabPage全般" );
 			this.tabPage全般.Name = "tabPage全般";
 			this.tabPage全般.UseVisualStyleBackColor = true;
 			// 
 			// checkBoxPlaySoundOnChip
 			// 
-			this.checkBoxPlaySoundOnChip.AccessibleDescription = null;
-			this.checkBoxPlaySoundOnChip.AccessibleName = null;
 			resources.ApplyResources( this.checkBoxPlaySoundOnChip, "checkBoxPlaySoundOnChip" );
-			this.checkBoxPlaySoundOnChip.BackgroundImage = null;
-			this.checkBoxPlaySoundOnChip.Font = null;
 			this.checkBoxPlaySoundOnChip.Name = "checkBoxPlaySoundOnChip";
 			this.checkBoxPlaySoundOnChip.UseVisualStyleBackColor = true;
 			// 
 			// checkBoxPreviewBGM
 			// 
-			this.checkBoxPreviewBGM.AccessibleDescription = null;
-			this.checkBoxPreviewBGM.AccessibleName = null;
 			resources.ApplyResources( this.checkBoxPreviewBGM, "checkBoxPreviewBGM" );
-			this.checkBoxPreviewBGM.BackgroundImage = null;
-			this.checkBoxPreviewBGM.Font = null;
 			this.checkBoxPreviewBGM.Name = "checkBoxPreviewBGM";
 			this.checkBoxPreviewBGM.UseVisualStyleBackColor = true;
 			// 
 			// checkBoxオートフォーカス
 			// 
-			this.checkBoxオートフォーカス.AccessibleDescription = null;
-			this.checkBoxオートフォーカス.AccessibleName = null;
 			resources.ApplyResources( this.checkBoxオートフォーカス, "checkBoxオートフォーカス" );
-			this.checkBoxオートフォーカス.BackgroundImage = null;
-			this.checkBoxオートフォーカス.Font = null;
 			this.checkBoxオートフォーカス.Name = "checkBoxオートフォーカス";
 			this.checkBoxオートフォーカス.UseVisualStyleBackColor = true;
 			// 
 			// label個まで表示する
 			// 
-			this.label個まで表示する.AccessibleDescription = null;
-			this.label個まで表示する.AccessibleName = null;
 			resources.ApplyResources( this.label個まで表示する, "label個まで表示する" );
-			this.label個まで表示する.Font = null;
 			this.label個まで表示する.Name = "label個まで表示する";
 			// 
 			// checkBox最近使用したファイル
 			// 
-			this.checkBox最近使用したファイル.AccessibleDescription = null;
-			this.checkBox最近使用したファイル.AccessibleName = null;
 			resources.ApplyResources( this.checkBox最近使用したファイル, "checkBox最近使用したファイル" );
-			this.checkBox最近使用したファイル.BackgroundImage = null;
-			this.checkBox最近使用したファイル.Font = null;
 			this.checkBox最近使用したファイル.Name = "checkBox最近使用したファイル";
 			this.checkBox最近使用したファイル.UseVisualStyleBackColor = true;
 			// 
 			// numericUpDown最近使用したファイルの最大表示個数
 			// 
-			this.numericUpDown最近使用したファイルの最大表示個数.AccessibleDescription = null;
-			this.numericUpDown最近使用したファイルの最大表示個数.AccessibleName = null;
 			resources.ApplyResources( this.numericUpDown最近使用したファイルの最大表示個数, "numericUpDown最近使用したファイルの最大表示個数" );
-			this.numericUpDown最近使用したファイルの最大表示個数.Font = null;
 			this.numericUpDown最近使用したファイルの最大表示個数.Maximum = new decimal( new int[] {
             10,
             0,
@@ -156,157 +130,104 @@
 			// 
 			// tabControlオプション
 			// 
-			this.tabControlオプション.AccessibleDescription = null;
-			this.tabControlオプション.AccessibleName = null;
 			resources.ApplyResources( this.tabControlオプション, "tabControlオプション" );
-			this.tabControlオプション.BackgroundImage = null;
 			this.tabControlオプション.Controls.Add( this.tabPage全般 );
 			this.tabControlオプション.Controls.Add( this.tabPageLanes );
 			this.tabControlオプション.Controls.Add( this.tabPageViewer );
-			this.tabControlオプション.Font = null;
 			this.tabControlオプション.Name = "tabControlオプション";
 			this.tabControlオプション.SelectedIndex = 0;
 			this.tabControlオプション.KeyDown += new System.Windows.Forms.KeyEventHandler( this.tabControlオプション_KeyDown );
 			// 
 			// tabPageLanes
 			// 
-			this.tabPageLanes.AccessibleDescription = null;
-			this.tabPageLanes.AccessibleName = null;
-			resources.ApplyResources( this.tabPageLanes, "tabPageLanes" );
-			this.tabPageLanes.BackgroundImage = null;
 			this.tabPageLanes.Controls.Add( this.labelSelectLanes );
 			this.tabPageLanes.Controls.Add( this.checkedListBoxLaneSelectList );
-			this.tabPageLanes.Font = null;
+			resources.ApplyResources( this.tabPageLanes, "tabPageLanes" );
 			this.tabPageLanes.Name = "tabPageLanes";
 			this.tabPageLanes.UseVisualStyleBackColor = true;
 			// 
 			// labelSelectLanes
 			// 
-			this.labelSelectLanes.AccessibleDescription = null;
-			this.labelSelectLanes.AccessibleName = null;
 			resources.ApplyResources( this.labelSelectLanes, "labelSelectLanes" );
-			this.labelSelectLanes.Font = null;
 			this.labelSelectLanes.Name = "labelSelectLanes";
 			// 
 			// checkedListBoxLaneSelectList
 			// 
-			this.checkedListBoxLaneSelectList.AccessibleDescription = null;
-			this.checkedListBoxLaneSelectList.AccessibleName = null;
-			resources.ApplyResources( this.checkedListBoxLaneSelectList, "checkedListBoxLaneSelectList" );
-			this.checkedListBoxLaneSelectList.BackgroundImage = null;
 			this.checkedListBoxLaneSelectList.CheckOnClick = true;
-			this.checkedListBoxLaneSelectList.Font = null;
 			this.checkedListBoxLaneSelectList.FormattingEnabled = true;
+			resources.ApplyResources( this.checkedListBoxLaneSelectList, "checkedListBoxLaneSelectList" );
 			this.checkedListBoxLaneSelectList.Name = "checkedListBoxLaneSelectList";
 			// 
 			// tabPageViewer
 			// 
-			this.tabPageViewer.AccessibleDescription = null;
-			this.tabPageViewer.AccessibleName = null;
-			resources.ApplyResources( this.tabPageViewer, "tabPageViewer" );
-			this.tabPageViewer.BackgroundImage = null;
 			this.tabPageViewer.Controls.Add( this.groupBox_SelectViewer );
-			this.tabPageViewer.Font = null;
+			resources.ApplyResources( this.tabPageViewer, "tabPageViewer" );
 			this.tabPageViewer.Name = "tabPageViewer";
 			this.tabPageViewer.UseVisualStyleBackColor = true;
 			// 
 			// groupBox_SelectViewer
 			// 
-			this.groupBox_SelectViewer.AccessibleDescription = null;
-			this.groupBox_SelectViewer.AccessibleName = null;
-			resources.ApplyResources( this.groupBox_SelectViewer, "groupBox_SelectViewer" );
-			this.groupBox_SelectViewer.BackgroundImage = null;
 			this.groupBox_SelectViewer.Controls.Add( this.groupBox_DTXManiaSettings );
 			this.groupBox_SelectViewer.Controls.Add( this.groupBox_SoundDeviceSettings );
 			this.groupBox_SelectViewer.Controls.Add( this.radioButton_UseDTXViewer );
 			this.groupBox_SelectViewer.Controls.Add( this.radioButton_UseDTXManiaGR );
-			this.groupBox_SelectViewer.Font = null;
+			resources.ApplyResources( this.groupBox_SelectViewer, "groupBox_SelectViewer" );
 			this.groupBox_SelectViewer.Name = "groupBox_SelectViewer";
 			this.groupBox_SelectViewer.TabStop = false;
 			// 
 			// groupBox_DTXManiaSettings
 			// 
-			this.groupBox_DTXManiaSettings.AccessibleDescription = null;
-			this.groupBox_DTXManiaSettings.AccessibleName = null;
-			resources.ApplyResources( this.groupBox_DTXManiaSettings, "groupBox_DTXManiaSettings" );
-			this.groupBox_DTXManiaSettings.BackgroundImage = null;
 			this.groupBox_DTXManiaSettings.Controls.Add( this.label1 );
 			this.groupBox_DTXManiaSettings.Controls.Add( this.checkBox_TimeStretch );
 			this.groupBox_DTXManiaSettings.Controls.Add( this.checkBox_VSyncWait );
 			this.groupBox_DTXManiaSettings.Controls.Add( this.checkBox_GRmode );
-			this.groupBox_DTXManiaSettings.Font = null;
+			resources.ApplyResources( this.groupBox_DTXManiaSettings, "groupBox_DTXManiaSettings" );
 			this.groupBox_DTXManiaSettings.Name = "groupBox_DTXManiaSettings";
 			this.groupBox_DTXManiaSettings.TabStop = false;
 			// 
 			// label1
 			// 
-			this.label1.AccessibleDescription = null;
-			this.label1.AccessibleName = null;
 			resources.ApplyResources( this.label1, "label1" );
-			this.label1.Font = null;
 			this.label1.Name = "label1";
 			// 
 			// checkBox_TimeStretch
 			// 
-			this.checkBox_TimeStretch.AccessibleDescription = null;
-			this.checkBox_TimeStretch.AccessibleName = null;
 			resources.ApplyResources( this.checkBox_TimeStretch, "checkBox_TimeStretch" );
-			this.checkBox_TimeStretch.BackgroundImage = null;
-			this.checkBox_TimeStretch.Font = null;
 			this.checkBox_TimeStretch.Name = "checkBox_TimeStretch";
 			this.checkBox_TimeStretch.UseVisualStyleBackColor = true;
 			// 
 			// checkBox_VSyncWait
 			// 
-			this.checkBox_VSyncWait.AccessibleDescription = null;
-			this.checkBox_VSyncWait.AccessibleName = null;
 			resources.ApplyResources( this.checkBox_VSyncWait, "checkBox_VSyncWait" );
-			this.checkBox_VSyncWait.BackgroundImage = null;
-			this.checkBox_VSyncWait.Font = null;
 			this.checkBox_VSyncWait.Name = "checkBox_VSyncWait";
 			this.checkBox_VSyncWait.UseVisualStyleBackColor = true;
 			// 
 			// checkBox_GRmode
 			// 
-			this.checkBox_GRmode.AccessibleDescription = null;
-			this.checkBox_GRmode.AccessibleName = null;
 			resources.ApplyResources( this.checkBox_GRmode, "checkBox_GRmode" );
-			this.checkBox_GRmode.BackgroundImage = null;
-			this.checkBox_GRmode.Font = null;
 			this.checkBox_GRmode.Name = "checkBox_GRmode";
 			this.checkBox_GRmode.UseVisualStyleBackColor = true;
 			// 
 			// groupBox_SoundDeviceSettings
 			// 
-			this.groupBox_SoundDeviceSettings.AccessibleDescription = null;
-			this.groupBox_SoundDeviceSettings.AccessibleName = null;
-			resources.ApplyResources( this.groupBox_SoundDeviceSettings, "groupBox_SoundDeviceSettings" );
-			this.groupBox_SoundDeviceSettings.BackgroundImage = null;
 			this.groupBox_SoundDeviceSettings.Controls.Add( this.label_Notice );
 			this.groupBox_SoundDeviceSettings.Controls.Add( this.radioButton_DirectSound );
 			this.groupBox_SoundDeviceSettings.Controls.Add( this.radioButton_WASAPI );
 			this.groupBox_SoundDeviceSettings.Controls.Add( this.comboBox_ASIOdevices );
 			this.groupBox_SoundDeviceSettings.Controls.Add( this.radioButton_ASIO );
-			this.groupBox_SoundDeviceSettings.Font = null;
+			resources.ApplyResources( this.groupBox_SoundDeviceSettings, "groupBox_SoundDeviceSettings" );
 			this.groupBox_SoundDeviceSettings.Name = "groupBox_SoundDeviceSettings";
 			this.groupBox_SoundDeviceSettings.TabStop = false;
 			// 
 			// label_Notice
 			// 
-			this.label_Notice.AccessibleDescription = null;
-			this.label_Notice.AccessibleName = null;
 			resources.ApplyResources( this.label_Notice, "label_Notice" );
 			this.label_Notice.AutoEllipsis = true;
-			this.label_Notice.Font = null;
 			this.label_Notice.Name = "label_Notice";
 			// 
 			// radioButton_DirectSound
 			// 
-			this.radioButton_DirectSound.AccessibleDescription = null;
-			this.radioButton_DirectSound.AccessibleName = null;
 			resources.ApplyResources( this.radioButton_DirectSound, "radioButton_DirectSound" );
-			this.radioButton_DirectSound.BackgroundImage = null;
-			this.radioButton_DirectSound.Font = null;
 			this.radioButton_DirectSound.Name = "radioButton_DirectSound";
 			this.radioButton_DirectSound.TabStop = true;
 			this.radioButton_DirectSound.UseVisualStyleBackColor = true;
@@ -314,11 +235,7 @@
 			// 
 			// radioButton_WASAPI
 			// 
-			this.radioButton_WASAPI.AccessibleDescription = null;
-			this.radioButton_WASAPI.AccessibleName = null;
 			resources.ApplyResources( this.radioButton_WASAPI, "radioButton_WASAPI" );
-			this.radioButton_WASAPI.BackgroundImage = null;
-			this.radioButton_WASAPI.Font = null;
 			this.radioButton_WASAPI.Name = "radioButton_WASAPI";
 			this.radioButton_WASAPI.TabStop = true;
 			this.radioButton_WASAPI.UseVisualStyleBackColor = true;
@@ -326,22 +243,14 @@
 			// 
 			// comboBox_ASIOdevices
 			// 
-			this.comboBox_ASIOdevices.AccessibleDescription = null;
-			this.comboBox_ASIOdevices.AccessibleName = null;
-			resources.ApplyResources( this.comboBox_ASIOdevices, "comboBox_ASIOdevices" );
-			this.comboBox_ASIOdevices.BackgroundImage = null;
 			this.comboBox_ASIOdevices.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-			this.comboBox_ASIOdevices.Font = null;
 			this.comboBox_ASIOdevices.FormattingEnabled = true;
+			resources.ApplyResources( this.comboBox_ASIOdevices, "comboBox_ASIOdevices" );
 			this.comboBox_ASIOdevices.Name = "comboBox_ASIOdevices";
 			// 
 			// radioButton_ASIO
 			// 
-			this.radioButton_ASIO.AccessibleDescription = null;
-			this.radioButton_ASIO.AccessibleName = null;
 			resources.ApplyResources( this.radioButton_ASIO, "radioButton_ASIO" );
-			this.radioButton_ASIO.BackgroundImage = null;
-			this.radioButton_ASIO.Font = null;
 			this.radioButton_ASIO.Name = "radioButton_ASIO";
 			this.radioButton_ASIO.TabStop = true;
 			this.radioButton_ASIO.UseVisualStyleBackColor = true;
@@ -349,11 +258,7 @@
 			// 
 			// radioButton_UseDTXViewer
 			// 
-			this.radioButton_UseDTXViewer.AccessibleDescription = null;
-			this.radioButton_UseDTXViewer.AccessibleName = null;
 			resources.ApplyResources( this.radioButton_UseDTXViewer, "radioButton_UseDTXViewer" );
-			this.radioButton_UseDTXViewer.BackgroundImage = null;
-			this.radioButton_UseDTXViewer.Font = null;
 			this.radioButton_UseDTXViewer.Name = "radioButton_UseDTXViewer";
 			this.radioButton_UseDTXViewer.TabStop = true;
 			this.radioButton_UseDTXViewer.UseVisualStyleBackColor = true;
@@ -361,11 +266,7 @@
 			// 
 			// radioButton_UseDTXManiaGR
 			// 
-			this.radioButton_UseDTXManiaGR.AccessibleDescription = null;
-			this.radioButton_UseDTXManiaGR.AccessibleName = null;
 			resources.ApplyResources( this.radioButton_UseDTXManiaGR, "radioButton_UseDTXManiaGR" );
-			this.radioButton_UseDTXManiaGR.BackgroundImage = null;
-			this.radioButton_UseDTXManiaGR.Font = null;
 			this.radioButton_UseDTXManiaGR.Name = "radioButton_UseDTXManiaGR";
 			this.radioButton_UseDTXManiaGR.TabStop = true;
 			this.radioButton_UseDTXManiaGR.UseVisualStyleBackColor = true;
@@ -373,39 +274,26 @@
 			// 
 			// button1
 			// 
-			this.button1.AccessibleDescription = null;
-			this.button1.AccessibleName = null;
 			resources.ApplyResources( this.button1, "button1" );
-			this.button1.BackgroundImage = null;
 			this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-			this.button1.Font = null;
 			this.button1.Name = "button1";
 			this.button1.UseVisualStyleBackColor = true;
 			// 
 			// buttonOK
 			// 
-			this.buttonOK.AccessibleDescription = null;
-			this.buttonOK.AccessibleName = null;
 			resources.ApplyResources( this.buttonOK, "buttonOK" );
-			this.buttonOK.BackgroundImage = null;
 			this.buttonOK.DialogResult = System.Windows.Forms.DialogResult.OK;
-			this.buttonOK.Font = null;
 			this.buttonOK.Name = "buttonOK";
 			this.buttonOK.UseVisualStyleBackColor = true;
 			// 
 			// Cオプションダイアログ
 			// 
-			this.AccessibleDescription = null;
-			this.AccessibleName = null;
 			resources.ApplyResources( this, "$this" );
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.BackgroundImage = null;
 			this.ControlBox = false;
 			this.Controls.Add( this.buttonOK );
 			this.Controls.Add( this.button1 );
 			this.Controls.Add( this.tabControlオプション );
-			this.Font = null;
-			this.Icon = null;
 			this.MaximizeBox = false;
 			this.MinimizeBox = false;
 			this.Name = "Cオプションダイアログ";
