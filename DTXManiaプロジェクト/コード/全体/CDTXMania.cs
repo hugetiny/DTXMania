f96d175df10a40595ae1f3e0a9806ee94894f4eb
yyagi <yyagi@16f42ceb-6dc6-49c8-ba94-f2d53467949d>
2015-08-15 17:26:56 +0000

    #35405 ギター、ベースのGボタンの自動演奏をオフにした状態でDTXCreator上で再生するとGボタンが自動演奏されない問題を修正。 #xxxxx バージョン情報を更新。 #xxxxx DTXManiaGR.exe.configファイルの更新時に、自動で「実行時フォルダ」のconfig1ファイルが連動して修正されるよう、csprojファイルを修正。(DTXC分は、前回のコミット時に同対応を実施済み)
    git-svn-id: http://svn.osdn.jp/svnroot/dtxmania/trunk@833 16f42ceb-6dc6-49c8-ba94-f2d53467949d

--- DTXManiaプロジェクト/コード/全体/CDTXMania.cs
+++ DTXManiaプロジェクト/コード/全体/CDTXMania.cs
@@ -20,7 +20,7 @@ namespace DTXMania
 	{
 		// プロパティ
 		#region [ properties ]
-		public static readonly string VERSION = "102(150808)";
+		public static readonly string VERSION = "103(150815)";
 		public static readonly string SLIMDXDLL = "c_net20x86_Jun2010";
 		public static readonly string D3DXDLL = "d3dx9_43.dll";		// June 2010
         //public static readonly string D3DXDLL = "d3dx9_42.dll";	// February 2010
