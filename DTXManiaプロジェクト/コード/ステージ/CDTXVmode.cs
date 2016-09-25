29076ff99c1fdf18340a4ccdc2778002400238f8
yyagi <yyagi.dtxmania@gmail.com>
2016-09-25 10:14:07 +0800

    #36612 DTXManiaでのViwerを実行にて、二回目以降の実行でウィンドウサイズがでかくなる問題を、今度こそ修正。

--- DTXManiaプロジェクト/コード/ステージ/CDTXVmode.cs
+++ DTXManiaプロジェクト/コード/ステージ/CDTXVmode.cs
@@ -382,6 +382,12 @@ namespace DTXMania
 		/// </summary>
 		public void tUpdateConfigIni()
 		{
+			/// Viewer関連の設定のみを更新するために、
+			/// 1. 現在のconfig.ini相当の情報を、別変数にコピーしておく
+			/// 2. config.iniを読み込みなおす
+			/// 3. 別変数のコピーから、Viewer関連の設定を、configに入れ込む
+			/// 4. Config.iniを保存する
+	
 			CConfigXml ConfigIni_backup = (CConfigXml) CDTXMania.Instance.ConfigIni.Clone();		// #36612 2016.9.12 yyagi
 			CDTXMania.Instance.LoadConfig();
 
@@ -418,8 +424,6 @@ namespace DTXMania
 			CDTXMania.Instance.ConfigIni.rcViewerWindow.H = ConfigIni_backup.rcWindow.H;
 			CDTXMania.Instance.ConfigIni.rcViewerWindow.X = ConfigIni_backup.rcWindow.X;
 			CDTXMania.Instance.ConfigIni.rcViewerWindow.Y = ConfigIni_backup.rcWindow.Y;
-
-			CDTXMania.Instance.ConfigIni.rcWindow = ConfigIni_backup.rcWindow_backup;		// #36612 2016.9.12 yyagi
 
 			CDTXMania.Instance.SaveConfig();
 
