a54ae3cb835e68249c7e02bd94c7d5724a758723
yyagi <yyagi.dtxmania@gmail.com>
2016-10-01 13:36:21 +0800

    #xxxxx 107のリリース番号更新。 #xxxxx .gitignoreファイルの更新。

--- DTXManiaプロジェクト/コード/全体/CDTXMania.cs
+++ DTXManiaプロジェクト/コード/全体/CDTXMania.cs
@@ -23,7 +23,7 @@ namespace DTXMania
 	{
 		// プロパティ
 		#region [ properties ]
-		public static readonly string VERSION = "107(160924)";
+		public static readonly string VERSION = "107(161001)";
 		public static readonly string SLIMDXDLL = "c_net20x86_Jun2010";
 		public static readonly string D3DXDLL = "d3dx9_43.dll";     // June 2010
 																																//public static readonly string D3DXDLL = "d3dx9_42.dll";	// February 2010
