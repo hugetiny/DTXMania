1a5478697ff55b2fc96eee866e38d4d73c9ed7aa
yyagi <yyagi.dtxmania@gmail.com>
2018-02-07 01:48:59 +0900

    #37946 MidiInChecker2: ここまでの修正で一区切りつけます。  * MIDIイベント毎の詳細出力情報を大幅に改善。なおNoteOn/Offの音階表示は国際式で表示しているが、内部的にはYAMAHA方式にも対応。(YAMAHA方式だとオクターブ表記が一段小さくなるだけだが)  * Fileメニューに、Save As(ファイルの保存)を追加。plaint textまたはrich textでの保存が可能。  * 右クリックメニューを追加(copy, select all)。同じものをメインメニューのEditにも追加。  * 改行コードを \r\nから System.Environment.NewLine に変更。  * なんとなく、ほぼすべてのコメントを英語化。 ちなみに日本語版の開発予定はありませんが、問題ないですよね？

--- MidiInChecker2_Solution/MidiInChecker2_Project/Form_Main.Designer.cs
+++ MidiInChecker2_Solution/MidiInChecker2_Project/Form_Main.Designer.cs
@@ -28,28 +28,54 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.components = new System.ComponentModel.Container();
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form_Main));
 			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
 			this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
 			this.exitToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+			this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.copyToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+			this.selectAllToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
 			this.buttonOK = new System.Windows.Forms.Button();
 			this.RichLogTextBox = new System.Windows.Forms.RichTextBox();
+			this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.pasteVToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
 			this.menuStrip1.SuspendLayout();
+			this.contextMenuStrip1.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// menuStrip1
 			// 
 			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.exitToolStripMenuItem});
+            this.exitToolStripMenuItem,
+            this.editToolStripMenuItem});
 			resources.ApplyResources(this.menuStrip1, "menuStrip1");
 			this.menuStrip1.Name = "menuStrip1";
 			// 
 			// exitToolStripMenuItem
 			// 
 			this.exitToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveAsToolStripMenuItem,
+            this.toolStripSeparator1,
             this.exitToolStripMenuItem1});
 			this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
 			resources.ApplyResources(this.exitToolStripMenuItem, "exitToolStripMenuItem");
+			// 
+			// saveAsToolStripMenuItem
+			// 
+			this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+			resources.ApplyResources(this.saveAsToolStripMenuItem, "saveAsToolStripMenuItem");
+			this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+			// 
+			// toolStripSeparator1
+			// 
+			this.toolStripSeparator1.Name = "toolStripSeparator1";
+			resources.ApplyResources(this.toolStripSeparator1, "toolStripSeparator1");
 			// 
 			// exitToolStripMenuItem1
 			// 
@@ -57,17 +83,71 @@
 			resources.ApplyResources(this.exitToolStripMenuItem1, "exitToolStripMenuItem1");
 			this.exitToolStripMenuItem1.Click += new System.EventHandler(this.exitToolStripMenuItem1_Click);
 			// 
+			// editToolStripMenuItem
+			// 
+			this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.copyToolStripMenuItem1,
+            this.toolStripSeparator2,
+            this.selectAllToolStripMenuItem});
+			this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+			resources.ApplyResources(this.editToolStripMenuItem, "editToolStripMenuItem");
+			// 
+			// copyToolStripMenuItem1
+			// 
+			this.copyToolStripMenuItem1.Name = "copyToolStripMenuItem1";
+			resources.ApplyResources(this.copyToolStripMenuItem1, "copyToolStripMenuItem1");
+			this.copyToolStripMenuItem1.Click += new System.EventHandler(this.copyToolStripMenuItem1_Click);
+			// 
+			// toolStripSeparator2
+			// 
+			this.toolStripSeparator2.Name = "toolStripSeparator2";
+			resources.ApplyResources(this.toolStripSeparator2, "toolStripSeparator2");
+			// 
+			// selectAllToolStripMenuItem
+			// 
+			this.selectAllToolStripMenuItem.Name = "selectAllToolStripMenuItem";
+			resources.ApplyResources(this.selectAllToolStripMenuItem, "selectAllToolStripMenuItem");
+			this.selectAllToolStripMenuItem.Click += new System.EventHandler(this.selectAllToolStripMenuItem_Click);
+			// 
 			// buttonOK
 			// 
 			resources.ApplyResources(this.buttonOK, "buttonOK");
 			this.buttonOK.Name = "buttonOK";
 			this.buttonOK.UseVisualStyleBackColor = true;
-			this.buttonOK.Click += new System.EventHandler(this.button1_Click);
+			this.buttonOK.Click += new System.EventHandler(this.button_OK_Click);
 			// 
 			// RichLogTextBox
 			// 
 			resources.ApplyResources(this.RichLogTextBox, "RichLogTextBox");
+			this.RichLogTextBox.ContextMenuStrip = this.contextMenuStrip1;
+			this.RichLogTextBox.HideSelection = false;
 			this.RichLogTextBox.Name = "RichLogTextBox";
+			// 
+			// contextMenuStrip1
+			// 
+			this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.copyToolStripMenuItem,
+            this.pasteVToolStripMenuItem});
+			this.contextMenuStrip1.Name = "contextMenuStrip1";
+			resources.ApplyResources(this.contextMenuStrip1, "contextMenuStrip1");
+			// 
+			// copyToolStripMenuItem
+			// 
+			this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+			resources.ApplyResources(this.copyToolStripMenuItem, "copyToolStripMenuItem");
+			this.copyToolStripMenuItem.Click += new System.EventHandler(this.copyToolStripMenuItem_Click);
+			// 
+			// pasteVToolStripMenuItem
+			// 
+			this.pasteVToolStripMenuItem.Name = "pasteVToolStripMenuItem";
+			resources.ApplyResources(this.pasteVToolStripMenuItem, "pasteVToolStripMenuItem");
+			this.pasteVToolStripMenuItem.Click += new System.EventHandler(this.pasteVToolStripMenuItem_Click);
+			// 
+			// saveFileDialog1
+			// 
+			resources.ApplyResources(this.saveFileDialog1, "saveFileDialog1");
+			this.saveFileDialog1.RestoreDirectory = true;
+			this.saveFileDialog1.SupportMultiDottedExtensions = true;
 			// 
 			// Form_Main
 			// 
@@ -82,6 +162,7 @@
 			this.Shown += new System.EventHandler(this.Form_Main_Shown);
 			this.menuStrip1.ResumeLayout(false);
 			this.menuStrip1.PerformLayout();
+			this.contextMenuStrip1.ResumeLayout(false);
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
@@ -93,6 +174,16 @@
 		private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem1;
 		private System.Windows.Forms.Button buttonOK;
 		private System.Windows.Forms.RichTextBox RichLogTextBox;
+		private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+		private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem1;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+		private System.Windows.Forms.ToolStripMenuItem selectAllToolStripMenuItem;
+		private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem pasteVToolStripMenuItem;
+		private System.Windows.Forms.SaveFileDialog saveFileDialog1;
 	}
 }
 
--- MidiInChecker2_Solution/MidiInChecker2_Project/Form_Main.cs
+++ MidiInChecker2_Solution/MidiInChecker2_Project/Form_Main.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Windows.Forms;
 using System.Diagnostics;
 using System.Threading;
+using System.IO;
 
 namespace MidiInChecker2
 {
@@ -15,30 +16,41 @@ namespace MidiInChecker2
 		CInputManager InputManager;
 		System.Threading.Timer timer;
 		object lockobj = new object();
-		int looptimes = 11;
+		int looptimes = 16;
+		bool bYAMAHA_way_note = false;
 
 		private void Form_Main_Shown(object sender, EventArgs e)
 		{
-			#region [ タイトルバーの設定 ]
+			#region [ Title bar configuration (append version info) ]
 			System.Reflection.Assembly asm = System.Reflection.Assembly.GetExecutingAssembly();
 			int ver_asm_major = asm.GetName().Version.Major;
 			this.Text = "MidiInChecker2 Rel" + ver_asm_major.ToString("D3");
 			#endregion
+			RichLogTextBox.LanguageOption = RichTextBoxLanguageOptions.UIFonts;
 		}
 
+		/// <summary>
+		/// Initializations
+		/// </summary>
 		public Form_Main()
 		{
 			InitializeComponent();
 			InputManager = new CInputManager();
 
-			RichLogTextBox.AppendText( "Number of MIDI devices: " + InputManager.nInputMidiDevices + "\r\n" );
+			RichLogTextBox.AppendText( "Number of MIDI devices: " + InputManager.nInputMidiDevices + System.Environment.NewLine );
 			foreach ( string s in InputManager.listStrMidiDevices )
 			{
-				RichLogTextBox.AppendText( s + "\r\n" );
+				RichLogTextBox.AppendText( s + System.Environment.NewLine );
 			}
-			RichLogTextBox.AppendText( "\r\nHit any MIDI Pad to show the signal info.\r\n\r\n\n" );
+			RichLogTextBox.AppendText(
+				System.Environment.NewLine +
+				"Hit any MIDI Pad to show the signal info." +
+				System.Environment.NewLine +
+				System.Environment.NewLine +
+				System.Environment.NewLine
+			);
 
-			#region [タイマーで0.1秒ごとにログ画面を更新するように初期化する]
+			#region [ Set callback to update log area (richtextbox) every 0.1sec ]
 			TimerCallback timerDelegate = new TimerCallback(mainloop);
 			timer = new System.Threading.Timer(timerDelegate, null , 0, 100);
 			#endregion
@@ -52,11 +64,12 @@ namespace MidiInChecker2
 				InputManager.tポーリング( true, true );
 				List<STInputEvent> list = new List<STInputEvent>();
 
-				// すべての入力デバイスについて…
+				// For all MIDI input devices...
 				foreach ( IInputDevice device in InputManager.list入力デバイス )
 				{
 					if ( ( device.list入力イベント != null ) && ( device.list入力イベント.Count != 0 ) && ( device.e入力デバイス種別 == E入力デバイス種別.MidiIn ) )
 					{
+						// if MIDI-input exists, invoke all of them, then push them to the RichTextBox
 						foreach ( STInputEvent ev in device.list入力イベント )
 						{
 							Invoke( new AppendTextDelegate( appendLogText ), device.ID, ev);
@@ -64,7 +77,7 @@ namespace MidiInChecker2
 						}
 					}
 				}
-				if (looptimes++ == 10)	// 10回ループ(1秒間)の間に入力がなければ、空行を挿入する。
+				if (looptimes++ == 15)	// Insert horizontal rule if no inputs in 15times-loop (1.5sec)
 				{
 					Invoke(new AppendTextDelegate( appendLogText ), -1, new STInputEvent() );
 				}
@@ -76,7 +89,7 @@ namespace MidiInChecker2
 		{
 			string text;
 
-			if (id == -1)
+			if (id == -1)	// Insert Horizontal Rule
 			{
 				RichLogTextBox.SelectionColor = Color.Black;    // reset color
 				text = "------------------------------------------------------------\r\n";
@@ -84,28 +97,89 @@ namespace MidiInChecker2
 			else
 			{
 				int nMIDIevent = ev.nKey & 0xFF;
-				int nNote = (ev.nKey >> 8) & 0xFF;    // note#
-													  // int nVelo = ( ev.nKey >> 16 ) & 0xFF;	// velocity
+				int nNote = (ev.nKey >> 8) & 0xFF;
+				string[] strNote =
+				{
+					"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"
+				};
 
-				DateTime dt = new DateTime(ev.nTimeStamp);
+				#region [ At first, put time, and MIDI event info ]
+				text = string.Format(
+					"{0}: Device={1}, MidiEvent=0x{2}",
+					new DateTime(ev.nTimeStamp).ToString("HH:mm:ss.fff"),
+					id,
+					nMIDIevent.ToString("X2")
+				);
+				#endregion
+				#region [ Then, Append MIDI event detail descriptions ]
+				switch (nMIDIevent & 0xF0)
+				{
+					case 0x80:	// Note off
+					case 0x90:	// Note off/on
+						text += string.Format(
+							", Note#=0x{0} (Key={1}{2}) {3}, Velocity={4}",
+							nNote.ToString("X2"),
+							strNote[nNote % 12],
+							(nNote / 12) - 1 - (bYAMAHA_way_note ? 1 : 0),
+							((nMIDIevent & 0xF0) == 0x80 || ev.nVelocity == 0x00) ? "Off" : "On ",
+							ev.nVelocity.ToString("D3")
+						);
+						break;
 
-				text = dt.ToString("hh:mm:ss.fff") +
-						": Device=" + id +
-						", MidiEvent=0x" + nMIDIevent.ToString("X2") +
-						", Note#=0x" + nNote.ToString("X2") +
-						", Velocity=" + ev.nVelocity.ToString("D3") +
-						"\r\n";
+					case 0xA0:  // Polyphonic Key Pressure
+						text += string.Format(
+							", Note#=0x{0} (Key={1}{2}), PolyphonicKeyPresssure={3}",
+							nNote.ToString("X2"),
+							strNote[nNote % 12],
+							(nNote / 12) - 1 - (bYAMAHA_way_note ? 1 : 0),
+							ev.nVelocity.ToString("D3")
+						);
+						break;
 
+					case 0xB0:  // Control Change
+						text += strCtrlChangeText(nNote, ev.nVelocity);
+						break;
+
+					case 0xC0:  // Program Change
+						text += string.Format(
+							", ChangeProgram to {0}",
+							nNote.ToString("X2")
+						);
+						break;
+
+					case 0xD0:  // Channel Pressure
+						text += string.Format(
+							", ChannelPresssure={0}",
+							nNote
+						);
+						break;
+
+					case 0xE0:  // Pitch Wheel Change
+						text += string.Format(
+							", PitchWheelChange to {0}",
+							((nNote + (ev.nVelocity * 128)) - 8192)
+						);
+						break;
+
+					case 0xF0:  // System Common Message
+						text += string.Format(
+							", SystemCommonMessage {0}",
+							nNote.ToString("X2")
+						);
+						break;
+				}
+				#endregion
+				#region [ Configure text color ]
 				Color[] cMidiEvent =
 				{
-					Color.Gray,			// 0x8x Note Off
-					Color.Black,		// 0x9x Note On
-					Color.Black,		// 0xAx
-					Color.Green,		// 0xBx Control change
-					Color.Black,		// 0xCx
-					Color.Black,		// 0xDx
-					Color.Blue,			// 0xEx Pitch bend
-					Color.Black,        // 0xFx
+					Color.Gray,			// 0x8x: Note Off
+					Color.Black,		// 0x9x: Note On
+					Color.Black,		// 0xAx: Poliphonic Key Pressure
+					Color.Green,		// 0xBx: Control change / Channel Mode Message
+					Color.Black,		// 0xCx: Program Change
+					Color.Black,		// 0xDx: Channel Pressure
+					Color.Blue,			// 0xEx: Pitch Wheel Change
+					Color.Purple,       // 0xFx: System Common Message
 				};
 				Color c = cMidiEvent[(nMIDIevent >> 4) - 8];
 				if ( (nMIDIevent & 0xF0) == 0x90 && (ev.nVelocity == 0) )
@@ -113,13 +187,14 @@ namespace MidiInChecker2
 					c = cMidiEvent[0];	// Note off color
 				}
 				RichLogTextBox.SelectionColor = c;
+				#endregion
 			}
 			RichLogTextBox.Focus();
-			RichLogTextBox.AppendText( text );
+			RichLogTextBox.AppendText( text + System.Environment.NewLine );
 			RichLogTextBox.SelectionColor = Color.Black;	// reset color
 		}
 
-		// ダサい。後日改善予定。
+		// Need improvement...
 		private void Form_Main_FormClosing( object sender, FormClosingEventArgs e )
 		{
 			// mainloop処理中なら、待つ
@@ -134,7 +209,7 @@ namespace MidiInChecker2
 			InputManager = null;
 		}
 
-		private void button1_Click( object sender, EventArgs e )
+		private void button_OK_Click( object sender, EventArgs e )
 		{
 			Application.Exit();
 		}
@@ -145,16 +220,208 @@ namespace MidiInChecker2
 		}
 
 		/// <summary>
-		/// textboxで、Ctrl-Aでの全選択ができるようにする
+		/// [Ctrl-A] => SelectAll
 		/// </summary>
 		/// <param name="sender"></param>
 		/// <param name="e"></param>
-		private void LogTextBox_KeyDown( object sender, KeyEventArgs e )
+		private void RichLogTextBox_KeyDown( object sender, KeyEventArgs e )
 		{
 			if ( e.KeyCode == System.Windows.Forms.Keys.A & e.Control == true )
 			{
 				RichLogTextBox.SelectAll();
 			} 
 		}
+
+		/// <summary>
+		/// Output logs about control change
+		/// </summary>
+		/// <param name="ctrl"></param>
+		/// <param name="data"></param>
+		/// <returns></returns>
+		private string strCtrlChangeText(int ctrl, int data)
+		{
+			#region [ Control Change Number => strings ]
+			string[] strCtrlChgName =
+			{
+			"BankSelect<MSB>",					// 0x00
+			"Modulation",
+			"BreathController",
+			"",
+			"FootController",
+			"PortamentTime",
+			"DataEntry",
+			"ChannelVolume",
+			"Balance",							// 0x08
+			"",
+			"Pan",
+			"Expression",
+			"EffectControl1",
+			"EffectControl2",
+			"",
+			"",
+			"GeneralPurposeControler1",			// 0x10
+			"GeneralPurposeControler2",			// 0x11
+			"GeneralPurposeControler3",			// 0x12
+			"GeneralPurposeControler4",			// 0x13
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"BankSelect<LSB>",					// 0x20
+			"Modulation<LSB>",
+			"BreathContoller<LSB>",
+			"",
+			"FootController<LSB>",
+			"PortamentTime<LSB>",
+			"DataEntry<LSB>",
+			"ChannelVOlume<LSB>",
+			"Balance<LSB>",
+			"",
+			"Expression<LSB>",
+			"Pan<LSB>",
+			"Expression<LSB>",
+			"EffectControl1<LSB>",
+			"EffectControl1<LSB>",
+			"",
+			"",
+			"GeneralPurposeContoller1<LSB>",	// 0x30
+			"GeneralPurposeContoller2<LSB>",	// 0x31
+			"GeneralPurposeContoller3<LSB>",	// 0x32
+			"GeneralPurposeContoller4<LSB>",	// 0x34
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"Hold1",							// 0x40
+			"Portament",
+			"Sostenuto",
+			"SoftPedal",
+			"LegatoFootswitch",
+			"Hold2",
+			"SoundVariation",
+			"Timbre/HarmonicIntensity",
+			"ReleaseTime",
+			"AttackTime",
+			"Brightness",
+			"DecayTime",
+			"VibratoTime",
+			"VibratoDepth",
+			"VibratoDelay",
+			"",
+			"GeneralPurposeController5",		// 0x50
+			"GeneralPurposeController6",		// 0x51
+			"GeneralPurposeController7",		// 0x52
+			"GeneralPurposeController8",		// 0x53
+			"PortamentContol",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"Effect1Depth(ReverbSendLevel)",
+			"Effect2Depth(TremoloDepth)",
+			"Effect3Depth(ChorusSendLevel)",
+			"Effect4Depth(CelesteDepth)",
+			"Effect5Depth(PhaserDepth)",
+			"DataIncrement",					// 0x60
+			"DataDecrement",
+			"NRPN(LSB)",
+			"NRPN(MSB)",
+			"RPN(LSB)",
+			"RPN(MSB",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",									// 0x70
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"",
+			"AllSoundOff",						// 0x78
+			"ResetAllControllers",
+			"LocalControl",
+			"AllNoteOff",
+			"OmniOff",
+			"OmniOn",
+			"Mono",
+			"Poly",									// 0x7F
+		};
+			#endregion
+
+			string ret = string.Format(
+				", CtrlChgNo=0x{0} ({1}), data={2}",
+				ctrl.ToString("D2"),
+				strCtrlChgName[ctrl],
+				data
+			);
+			
+			return ret;
+		}
+
+		/// <summary>
+		/// Save Logs to the file (txt / rtf)
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+			{
+				string filename = saveFileDialog1.FileName;
+				bool bRTF = (Path.GetExtension(filename).ToLower() == ".rtf");
+
+				RichLogTextBox.SaveFile(
+					filename,
+					bRTF ? RichTextBoxStreamType.RichText : RichTextBoxStreamType.PlainText
+				);
+			}
+		}
+
+		#region [ Copy/SelectAll support ]
+		private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			RichLogTextBox.Copy();
+		}
+
+		private void pasteVToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			RichLogTextBox.SelectAll();
+		}
+
+		private void copyToolStripMenuItem1_Click(object sender, EventArgs e)
+		{
+			RichLogTextBox.Copy();
+		}
+
+		private void selectAllToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			RichLogTextBox.SelectAll();
+		}
+		#endregion
 	}
 }
