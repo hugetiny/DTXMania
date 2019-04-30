00d674ea86e4714130e596edb1e47ca9e4f3642e
yyagi <yyagi.dtxmania@gmail.com>
2019-04-30 22:41:03 +0900

    #39180 ベースのRとBが重なって表示される問題を修正。

--- DTXMania/コード/ステージ/04.コンフィグ/CConfigXml.cs
+++ DTXMania/コード/ステージ/04.コンフィグ/CConfigXml.cs
@@ -792,7 +792,7 @@ namespace DTXMania
 
 			cdLaneOrder.BsR.Value = 0;
 			cdLaneOrder.BsG.Value = 1;
-			cdLaneOrder.GtB.Value = 2;
+			cdLaneOrder.BsB.Value = 2;
 			cdLaneOrder.BsW.Value = 3;
 
 			bConfigIniが存在している = System.IO.File.Exists(CDTXMania.Instance.strEXEのあるフォルダ + "Config.xml");
